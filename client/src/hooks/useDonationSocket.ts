9bac53aa820ab0ba102b6e941ab47d681cb8471a
Andi Rayka <andi.rayka@gmail.com>
2025-04-13 07:58:58 +0700

    refactor(useDonationSocket): replace socket import with direct io initialization

--- client/src/hooks/useDonationSocket.ts
+++ client/src/hooks/useDonationSocket.ts
@@ -1,15 +1,22 @@
 import { useEffect } from "react";
-import { socket } from "../utils/socket";
+import { io } from "socket.io-client";
 import { useDonationStore } from "../store/donationStore";
+
+const socket = io("http://localhost:3000");
 
 export const useDonationSocket = () => {
   const addDonation = useDonationStore((s) => s.addDonation);
 
   useEffect(() => {
-    socket.on("new_donation", addDonation);
+    socket.on("new_donation", (donation) => {
+      console.log("💸 Received donation:", donation);
+      addDonation(donation);
+    });
 
     return () => {
       socket.off("new_donation", addDonation);
     };
   }, [addDonation]);
+
+  return socket;
 };
